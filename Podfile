15f84be4e53543fd60f90cfeb8f2dc41f3b32ea1
Carlos Diaz <carlosdz.centi@gmail.com>
2021-09-09 20:47:28 -0500

    Base commint

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '13.0'
+
+target 'PronacaShareholders' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PronacaShareholders
+  pod 'Alamofire', '~> 5.2'
+  pod 'Kingfisher', '6.0.1'
+
+end
---
+++ PronacaShareholders/Common/Extension/UIColor+Extension.swift
@@ -0,0 +1,52 @@
+//
+//  UIColor+Extension.swift
+//  PronacaShareholders
+//
+//  Created by CarlosDz on 9/09/21.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - RED -
+    
+    /// #D03A2B
+    static var red1: UIColor {
+        return UIColor(red: 208, green: 58, blue: 43, alpha: 1.0)
+    }
+    
+    /// #FCF6F6
+    static var red246: UIColor {
+        return UIColor(red: 255, green: 246, blue: 246, alpha: 1.0)
+    }
+    
+    // MARK: - BLACK -
+    
+    /// #191919
+    static var black0: UIColor {
+        return UIColor(red: 25, green: 25, blue: 25, alpha: 1.0)
+    }
+    
+    /// #424D4F
+    static var black1: UIColor {
+        return UIColor(red: 66, green: 77, blue: 79, alpha: 1.0)
+    }
+    
+    /// #757B7D
+    static var black2: UIColor {
+        return UIColor(red: 117, green: 123, blue: 125, alpha: 1.0)
+    }
+    
+    // MARK: - GREEN -
+    
+    /// #87BA41
+    static var green1: UIColor {
+        return UIColor(red: 135, green: 186, blue: 65, alpha: 1.0)
+    }
+    
+    /// #629B3F
+    static var green2: UIColor {
+        return UIColor(red: 98, green: 155, blue: 63, alpha: 1.0)
+    }
+}
